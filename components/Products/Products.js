cceea1b87c6b73877c503d9ebcd55194df86868b
Malik Duisebayev <malik.duisebaev@mail.ru>
2022-07-06 11:30:24 +0600

    feat: Products page init

---
+++ components/Products/Products.js
@@ -0,0 +1,20 @@
+class Products {
+  render() {
+    let htmlCatalog = "";
+    CATALOG.forEach(({ id, name, price, img }) => {
+      htmlCatalog += `
+            <li class="products-element"> 
+                <span class="products-element__name"> ${name} </span>
+                <img class="products-element__img" src="${img}">
+                <span class="products-element__price"> 💰 ${price.toLocaleString()} USD </span>
+                <button class="products-element__btn">Добавить в корзину</button>
+            </li>
+      `;
+      const html = `<ul class = "products-container"> ${htmlCatalog} </ul>`;
+      ROOT_PRODUCTS.innerHTML = html;
+    });
+  }
+}
+
+const productsPage = new Products();
+productsPage.render();
---
+++ constants/root.js
@@ -0,0 +1,3 @@
+const ROOT_PRODUCTS = document.querySelector("#products");
+const ROOT_HEADER = document.querySelector("#header");
+const ROOT_SHOPPING = document.querySelector("#shopping");
